b8545ba1a872fcb194b9bb431c3777f5659fe51a
Frank <fanhongxiang13@mails.ucas.ac.cn>
2019-02-05 20:38:57 +0800

    implement plyr::rename

--- R/set_colnames.R
+++ R/set_colnames.R
@@ -1,3 +1,4 @@
+#' @importFrom plyr rename
 NULL
 #' Set column names for each dataset.
 #' @param x  Dataset to be processed.
@@ -10,137 +11,137 @@ set_colnames <- function(x, ...){
 #' @rdname set_colnames
 #' @export
 set_colnames.PRS <- function(x, ...){
-  colnames(x) <<- c("ID",
-                   "Lat",
-                   "Lon",
-                   "H",
-                   "Year",
-                   "Month",
-                   "Day",
-                   "AvePRS",
-                   "MaxPRS",
-                   "MinPRS",
-                   "PRSCODE1",
-                   "PRSCODE2",
-                   "PRSCODE3")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "AvePRS",
+                              "V9" = "MaxPRS",
+                              "V10" = "MinPRS",
+                              "V11" = "PRSCODE1",
+                              "V12" = "PRSCODE2",
+                              "V13" = "PRSCODE3"))
 }
 
 #' @rdname set_colnames
 #' @export
 set_colnames.TEM <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "AveTEM",
-                    "MaxTEM",
-                    "MinTEM",
-                    "TEMCODE1",
-                    "TEMCODE2",
-                    "TEMCODE3")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "AveTEM",
+                              "V9" = "MaxTEM",
+                              "V10" = "MinTEM",
+                              "V11" = "TEMCODE1",
+                              "V12" = "TEMCODE2",
+                              "V13" = "TEMCODE3"))
 }
 
 #' @rdname set_colnames
 #' @export
 set_colnames.RHU <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "AveRHU",
-                    "MinRHU",
-                    "RHUCODE1",
-                    "RHUCODE2")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "AveRHU",
+                              "V9" = "MinRHU",
+                              "V10" = "RHUCODE1",
+                              "V11" = "RHUCODE2"))
 }
 
 #' @rdname set_colnames
 #' @export
 set_colnames.PRE <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "20_8PRE",
-                    "8_20PRE",
-                    "20_20PRE",
-                    "PRECODE1",
-                    "PRECODE2",
-                    "PRECODE3",
-                    "PRECODE4")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "20_8PRE",
+                              "V9" = "8_20PRE",
+                              "V10" = "20_20PRE",
+                              "V11" = "PRECODE1",
+                              "V12" = "PRECODE2",
+                              "V13" = "PRECODE3",
+                              "V14" = "PRECODE4"))
 }
 
 #' @rdname set_colnames
 #' @export
 set_colnames.EVP <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "SEVP",
-                    "LEVP",
-                    "EVPCODE1",
-                    "EVPCODE2")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "SEVP",
+                              "V9" = "LEVP",
+                              "V10" = "EVPCODE1",
+                              "V11" = "EVPCODE2"))
 }
 
 #' @rdname set_colnames
 #' @export
 set_colnames.WIN <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "AveWINS",
-                    "MaxWINS",
-                    "MaxWIND",
-                    "ExtWINS",
-                    "ExtWIND",
-                    "WINCODE1",
-                    "WINCODE2",
-                    "WINCODE3",
-                    "WINCODE4",
-                    "WINCODE5")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "AveWINS",
+                              "V9" = "MaxWINS",
+                              "V10" = "MaxWIND",
+                              "V11" = "ExtWINS",
+                              "V12" = "ExtWIND",
+                              "V13" = "WINCODE1",
+                              "V14" = "WINCODE2",
+                              "V15" = "WINCODE3",
+                              "V16" = "WINCODE4",
+                              "V17" = "WINCODE5"))
 }
 
 set_colnames.SSD <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "SSD",
-                    "SSDCODE1")
+ plyr::rename(x, replace = c("V1" = "ID",
+                             "V2" = "Lat",
+                             "V3" = "Lon",
+                             "V4" = "H",
+                             "V5" = "Year",
+                             "V6" = "Month",
+                             "V7" = "Day",
+                             "V8" = "SSD",
+                             "V9" = "SSDCODE1"))
 }
 
 set_colnames.GST <- function(x, ...){
-  colnames(x) <<- c("ID",
-                    "Lat",
-                    "Lon",
-                    "H",
-                    "Year",
-                    "Month",
-                    "Day",
-                    "AveGST",
-                    "MaxGST",
-                    "MinGST",
-                    "GSTCODE1",
-                    "GSTCODE2",
-                    "GSTCODE3")
+  plyr::rename(x, replace = c("V1" = "ID",
+                              "V2" = "Lat",
+                              "V3" = "Lon",
+                              "V4" = "H",
+                              "V5" = "Year",
+                              "V6" = "Month",
+                              "V7" = "Day",
+                              "V8" = "AveGST",
+                              "V9" = "MaxGST",
+                              "V10" = "MinGST",
+                              "V11" = "GSTCODE1",
+                              "V12" = "GSTCODE2",
+                              "V13" = "GSTCODE3"))
 }
 
