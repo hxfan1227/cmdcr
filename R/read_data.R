1d649dcd498575eb45e64338f708ed7d6dd33e81
Frank <fanhongxiang13@mails.ucas.ac.cn>
2019-02-07 10:59:16 +0800

    delete unused argument in read_data()

--- R/read_data.R
+++ R/read_data.R
@@ -2,7 +2,7 @@
 NULL
 #' Read the meteorological data.
 #' @param fname Character. Full name of the file to be processed.
-#' @param as.data.table Logical. If \code{True}, a data.table will return.
+#' @return A \code{cmdc} object.
 #' @export
 read_data <- function(fname){
   cmdc <- cmdcr::import_cmdc_data(fname)
