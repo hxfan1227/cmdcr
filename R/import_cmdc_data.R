60b85ec694abde3e02e1dd22807d7d156d35be65
Hongxiang Fan <hxfan@niglas.ac.cn>
2024-10-28 12:40:01 +0800

    add fill = T in fread

--- R/import_cmdc_data.R
+++ R/import_cmdc_data.R
@@ -6,7 +6,7 @@ NULL
 #' @export
 import_cmdc_data <- function(fname){
   cmdc <- list()
-  cmdc$data <- data.table::fread(fname)
+  cmdc$data <- data.table::fread(fname, fill = T) # in case that the row lengths are not equal.
   class(cmdc) <- cmdcr::get_data_type(fname)
   cmdc
 }
